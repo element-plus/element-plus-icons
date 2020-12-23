414c0a1b7208183521dcc8900868de7614da7211
JeremyWuuuuu <591449570@qq.com>
2020-12-23 15:03:34 +0800

    fix variables

--- publish.sh
+++ publish.sh
@@ -5,6 +5,6 @@
 rm -rf build
 yarn build
 
-cat package.json | grep -v '"version":' | sed 's/\("name": "@element-plus\/icons"\)/\1,\n  "version": "${TAG_VERSION}"/g' > build/package.json
+cat package.json | grep -v '"version":' | sed "s/\(\"name\": \"@element-plus\/icons\"\)/\1,\n  \"version\": \"${TAG_VERSION}\"/g" > build/package.json
 
 npm publish "./build" --registry ${REGISTRY}

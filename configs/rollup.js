25c1f0b454bbcb3b0d283d7278e5642f58919d90
JeremyWuuuuu <591449570@qq.com>
2020-12-29 11:45:06 +0800

    feat/add-index-min-js - Add index.min.js - Add terser plugin for exporting index.min.js

--- configs/rollup.js
+++ configs/rollup.js
@@ -1,27 +1,46 @@
-const path = require('path')
-const rollup = require('rollup')
-const typescript = require('rollup-plugin-typescript2')
+const path = require("path");
+const rollup = require("rollup");
+const typescript = require("rollup-plugin-typescript2");
+const { terser } = require("rollup-plugin-terser");
 
-const root = path.resolve(__dirname, '..');
+const root = path.resolve(__dirname, "..");
 const esm = {
-  file: path.resolve(root, './build/es/index.js'),
-  format: 'esm',
-  name: 'icons',
+  file: path.resolve(root, "./build/es/index.js"),
+  format: "esm",
+  name: "icons",
   sourcemap: false,
 };
 
 const cjs = {
-  file: path.resolve(root, './build/lib/index.js'),
-  format: 'cjs',
-  exports: 'named',
+  file: path.resolve(root, "./build/lib/index.js"),
+  format: "cjs",
+  exports: "named",
   sourcemap: false,
-}
+};
 
-rollup.rollup({
-  input: path.resolve(root, './packages/index.ts'),
-  plugins: [typescript()],
-  external: ['vue']
-}).then(async bundle => {
-  await Promise.all([bundle.write(esm), bundle.write(cjs)]);
-  console.log('full bundle built success');
-});
+const minified = {
+  file: path.resolve(root, "./build/lib/index.min.js"),
+  format: "cjs",
+  exports: "named",
+  plugins: [terser({
+    output: {
+      comments: () => false,
+    }
+  })],
+  sourcemap: false,
+};
+
+rollup
+  .rollup({
+    input: path.resolve(root, "./packages/index.ts"),
+    plugins: [typescript()],
+    external: ["vue"],
+  })
+  .then(async (bundle) => {
+    await Promise.all([
+      bundle.write(esm),
+      bundle.write(cjs),
+      bundle.write(minified),
+    ]);
+    console.log("full bundle built success");
+  });

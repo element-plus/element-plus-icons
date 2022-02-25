67f271db589b70f2595c318e83819f130bdebc3b
Kevin <sxzz@sxzz.moe>
2022-02-25 21:43:09 +0800

    build: remove umd, add iife and cjs

--- packages/vue/build/build.ts
+++ packages/vue/build/build.ts
@@ -6,46 +6,63 @@ import vue from 'unplugin-vue/rollup'
 import { emptyDir } from 'fs-extra'
 import { pathOutput, pathSrc } from './paths'
 
-const build = async () => {
-  const bundle = await rollup({
+const getBundle = (minify: boolean) =>
+  rollup({
     input: [path.resolve(pathSrc, 'index.ts')],
     plugins: [
       vue(),
       esbuild({
         target: 'es2018',
+        minify,
       }),
     ],
     external: ['vue'],
   })
-  await Promise.all([
+
+const buildBundled = async (minify: boolean) => {
+  const bundle = await getBundle(minify)
+  const tasks = [
     bundle.write({
-      format: 'es',
-      dir: path.resolve(pathOutput, 'es'),
-      preserveModules: true,
-      entryFileNames: '[name].mjs',
-    }),
-    bundle.write({
-      format: 'cjs',
-      dir: path.resolve(pathOutput, 'lib'),
-      preserveModules: true,
-      exports: 'named',
-    }),
-    bundle.write({
-      format: 'umd',
-      file: path.resolve(pathOutput, 'index.js'),
+      format: 'iife',
+      file: path.resolve(pathOutput, `index.iife${minify ? '.min' : ''}.js`),
       name: 'ElementPlusIconsVue',
       globals: { vue: 'Vue' },
     }),
-    bundle.write({
-      format: 'esm',
-      file: path.resolve(pathOutput, 'index.mjs'),
-    }),
-  ])
+  ]
+  if (!minify)
+    tasks.push(
+      bundle.write({
+        format: 'cjs',
+        file: path.resolve(pathOutput, `index${minify ? '.min' : ''}.js`),
+        globals: { vue: 'Vue' },
+      }),
+      bundle.write({
+        format: 'esm',
+        file: path.resolve(pathOutput, `index${minify ? '.min' : ''}.mjs`),
+      })
+    )
+  await Promise.all(tasks)
+}
+
+const buildUnbundled = async () => {
+  const bundle = await getBundle(false)
+  bundle.write({
+    format: 'es',
+    dir: path.resolve(pathOutput, 'es'),
+    preserveModules: true,
+    entryFileNames: '[name].mjs',
+  })
+  bundle.write({
+    format: 'cjs',
+    dir: path.resolve(pathOutput, 'lib'),
+    preserveModules: true,
+    exports: 'named',
+  })
 }
 
 ;(async () => {
   console.info(chalk.blue('cleaning dist...'))
   await emptyDir(pathOutput)
   console.info(chalk.blue('building...'))
-  await build()
+  await Promise.all([buildUnbundled(), buildBundled(true), buildBundled(false)])
 })()

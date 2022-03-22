3c7cfc9a1d36f57ce3a362acea30fbbce53c043f
三咲智子 <sxzz@sxzz.moe>
2022-03-23 00:31:11 +0800

    chore: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ const { defineConfig } = require('eslint-define-config')
 module.exports = defineConfig({
   extends: ['@sxzz'],
   rules: {
-    'import/named': 'off',
     'vue/multi-word-component-names': 'off',
   },
 })

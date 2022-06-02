2778d6f4f5e6d5c94676ec3b1d5d28ac28ae8d7f
liguanliang1 <liguanliang1@sensetime.com>
2022-06-02 10:41:15 +0800

    fix: fix the error when options is  or undefined, but the prefix is undefined

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -4,9 +4,10 @@ import type { App } from 'vue'
 
 interface InstallOptions {
   /** @default `ElIcon` */
-  prefix?: string
+  prefix?: string;
 }
-export default (app: App, { prefix = 'ElIcon' }: InstallOptions = {}) => {
+export default (app: App, options: InstallOptions = {}) => {
+  const { prefix = 'ElIcon' } = options;
   for (const [key, component] of Object.entries(icons)) {
     app.component(prefix + key, component)
   }

a31217fb58de0e924f0ecd69577a36c307f264e7
三咲智子 <sxzz@sxzz.moe>
2022-06-04 19:13:55 +0800

    fix(vue): types

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import ElIcons from '@element-plus/icons-vue'
+import ElIcons from '@element-plus/icons-vue/global'
 import App from './App.vue'
 
 createApp(App).use(ElIcons).mount('#app')
